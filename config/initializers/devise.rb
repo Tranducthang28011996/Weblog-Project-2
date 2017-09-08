defc8d9bb89b9b079a016ed9964eb80568fa1473
TDT <tieuhodo96@EMAIL.com>
2017-09-08 10:05:57 +0700

    devise-config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = Settings.email
 
   require "devise/orm/active_record"
 
@@ -12,7 +12,8 @@ Devise.setup do |config|
   config.expire_all_remember_me_on_sign_out = true
   config.password_length = 6..128
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
-  config.reset_password_within = 6.hours
+  config.reset_password_within = Settings.,reset_passwordhours
   config.sign_out_via = :delete
+  config.confirm_within = Settings.confirm.days
 
 end
